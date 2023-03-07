2bbb50f7d710f33734d2509700f0261b84c5d357
Joseph O <on.josephinfo@gmail.com>
2023-03-07 20:09:20 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -68,29 +68,3 @@ CREATE INDEX owners_asc ON owners(email desc) INCLUDE (id, full_name, age);
 
 -- Add email column to owners table
 ALTER TABLE animals ADD COLUMN email VARCHAR(120);
-
--- Create index to optimize queries
-create index id_btree1 on visits using btree(animal_id) where animal_id = 4;
-
-create index vets_asc1 on visits(vet_id asc) include (animal_id, date_of_visit);
-
-create index owners_asc1 on owners(email desc) include (id, full_name, age);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
